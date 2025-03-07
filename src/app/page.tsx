4e6aef3f30d1b53b1ec8ffa80bffc27fa643b850
Robee Herrera <bee.herrera215@gmail.com>
2025-03-07 18:18:41 +0800

    implement landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,50 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col relative w-screen h-screen">
+      <div className="flex flex-row max-w-auto h-24 bg-white items-center p-8">
+        <span className="text-black blur-none">Header</span>
+      </div>
+      <div className="flex items-center justify-center relative top-0 left-0 w-full h-full">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/neu-camp-1.png"
+          alt="neu-campus-background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="flex flex-col gap-y-12 items-center justify-center h-1/2 w-1/2 z-10">
+          <Image
+            src="/image.png"
+            alt="neu-campus-background"
+            width={300}
+            height={300}
+            objectFit="cover"
+            quality={100}
+          />
+          <Button
+            variant="outline"
+            size="lg"
+            className="bg-black border-black text-white w-1/4 hover:bg-gray-800 hover:text-white "
+            onClick={() => router.push("/auth/login")}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Get Started
+          </Button>
+          <div className="w-3/4">
+            <p className="text-center font-semibold">
+              UniTraffic ensures hassle-free vehicle entry at New Era University by verifying
+              sticker registration and tracking violations with a quick scan.
+            </p>
+          </div>
+          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-65 -z-10" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <Link href="/auth/login">
-          <button
-          className="w-75 p-2 m-3  border border-gray-500 rounded-md flex items-center justify-center gap-2 shadow-md hover:opacity-70 cursor-pointer"
-          >LOGIN</button>
-        </Link>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
