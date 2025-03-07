ce9b9a48aa98a8ac2c8df9c7d6f85742321d1e04
Arvicss <ramosarvyx@gmail.com>
2025-03-08 07:45:39 +0800

    add header to landing page cleanup code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,24 @@
 "use client";
 
-import Image from "next/image";
+import { Header } from "@/components/common/header";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
   return (
-    <div className="flex flex-col relative w-screen h-screen">
-      <div className="flex flex-row max-w-auto h-24 bg-white items-center p-8">
-        <span className="text-black blur-none">Header</span>
-      </div>
-      <div className="flex items-center justify-center relative top-0 left-0 w-full h-full">
-        <Image
-          src="/neu-camp-1.png"
-          alt="neu-campus-background"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
-        <div className="flex flex-col gap-y-12 items-center justify-center h-1/2 w-1/2 z-10">
-          <Image
-            src="/image.png"
-            alt="neu-campus-background"
-            width={300}
-            height={300}
-            objectFit="cover"
-            quality={100}
-          />
-          <Button
-            variant="outline"
-            size="lg"
-            className="bg-black border-black text-white w-1/4 hover:bg-gray-800 hover:text-white "
-            onClick={() => router.push("/auth/login")}
-          >
-            Get Started
-          </Button>
-          <div className="w-3/4">
-            <p className="text-center font-semibold">
-              UniTraffic ensures hassle-free vehicle entry at New Era University by verifying
-              sticker registration and tracking violations with a quick scan.
-            </p>
-          </div>
-          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-65 -z-10" />
+    <div
+      className="w-screen h-screen bg-[url('/neuCampusBlurred.png')] overflow-hidden bg-cover bg-center bg-no-repeat ] flex flex-col">
+      <Header/>
+      <div className="flex w-full h-full justify-center items-center">
+        <div
+          className="flex flex-col items-center justify-center gap-y-20 text-center">
+          <div className="bg-[url('/uniTrafficLogo.png')] bg-cover bg-center bg-no-repeat w-[300px] h-[189px]"/>
+          <Button className="w-[12vw] min-w-[129px]" asChild><Link href="/auth/login">Get Started</Link></Button>
+          <p className="max-w-[600px] text-sm text-black">
+            UniTraffic ensures hassle-free vehicle entry at New Era University
+            by verifying sticker registration and tracking violations with a
+            quick scan.
+          </p>
         </div>
       </div>
     </div>
